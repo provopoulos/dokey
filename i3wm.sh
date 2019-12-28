be8c745beeb2cb1775bf5ef00e0d4483a54f3191
V. Provopoulos <56654653+provopoulos@users.noreply.github.com>
2019-12-28 20:30:15 +0200

    Update i3wm.sh

--- i3wm.sh
+++ i3wm.sh
@@ -3,7 +3,7 @@
 sudo /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
 sudo dpkg -i ./keyring.deb
 sudo -i 
-sudo echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 exit
 sudo apt-get update
 sudo apt-get install i3
